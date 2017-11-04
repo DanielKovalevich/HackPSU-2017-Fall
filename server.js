2f5836fade169abd7e4e6b78625904ef42a0da7c
Daniel Kovalevich <dkoval526@gmail.com>
2017-11-04 11:56:13 -0400

    Created working proxy

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
-var test = require('./routes/test');
+var example = require('./routes/example');
 var users = require('./routes/users');
 
 var app = express();
