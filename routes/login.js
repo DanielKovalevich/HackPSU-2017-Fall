c24113f9898fe5ef39b94d7024d62e30d04a59c9
Daniel Kovalevich <dkoval526@gmail.com>
2017-11-04 17:01:24 -0400

    Random

--- routes/login.js
+++ routes/login.js
@@ -1,14 +1,18 @@
 var express = require('express');
+var db = require('../database/database.js');
 
 var router = express.Router();
 
-/* GET test function. */
 router.get('/', function(req, res, next) {
-  console.log("TEST FUNCTION");
+	var username = req.body.username;
 
-  
+	var query = 'SELECT username FROM UserTable WHERE username=' + username;
+	
+	db.get(query, (rows, fields) => {
+		console.log(rows);
+	});
 
-  res.json({message: 'Fuck off'});
+	res.json({message: 'Fuck off'});
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var bodyParser = require('body-parser');
 var index = require('./routes/index');
 var example = require('./routes/example');
 var users = require('./routes/users');
-var signup = require('./routes/signup');
+var login = require('./routes/login');
 
 var app = express();
 
@@ -27,7 +27,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', index);
 app.use('/example', example);
 app.use('/users', users);
-app.use('/signup', signup);
+app.use('/login', login);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
