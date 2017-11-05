87dafd668e8f9a0713d0e89ff3d83c74d341e7c9
Daniel Kovalevich <dkoval526@gmail.com>
2017-11-05 01:26:29 -0400

    whatever

--- client/public/js/index.js
+++ client/public/js/index.js
@@ -1,20 +1,35 @@
 $(document).ready(function() {
+    getCurrentUserData();
     demo.initDashboardPageCharts();
 
     var d = new Date();
-    
     var date = d.getMonth().toString() + '/' + d.getDay().toString() + '/' + d.getFullYear().toString();
-
     $("#time").html("<i class=\"material-icons\">access_time</i> " + new Date().toString() + "");
     $("#time2").html(date);
-    $('.loggout').click(function() {window.location = "./login.html";});
-    $('#myModal').modal('show');
 
-    $(".buy-stock").click(function() {
-        var $item = $(this).closest("tr")   // Finds the closest row <tr> 
-                           .find(".nr")     // Gets a descendent with class="nr"
-                           .text();         // Retrieves the text within <td>
-    
-        alert($item);       // Outputs the answer
-    });
+    $('.loggout').click(function() {window.location = "./login.html";});
+
+    $('#myModal').modal('show');
 });
+
+function getCurrentUserData() {
+    $.ajax({
+        url: "defaultUser",
+        type: "get",
+        // Manipulate data here.
+        success: function(data) {
+            console.log(data);
+            $('#userTitle').html(data.username);
+
+            var stocks = data.stocks.split(",");
+            var shares = data.stock_shares.split(",");
+
+            for(var i = 0; i < stocks.length; i++) {
+                $('#stock-body').append('<tr><td>' + stocks[i] + '</td><td> TESTING </td><td>' + shares[i] + '</td></tr>');
+            }
+        },
+        error: function (req, text_status, error) {
+            console.log('Error: ' + error.message);
+        }
+    });
+}
