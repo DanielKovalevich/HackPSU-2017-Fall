0ea7686221b48a17da99f7b4b2840ee7eb34c039
Daniel Kovalevich <dkoval526@gmail.com>
2017-11-05 09:00:23 -0500

    server stuff

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ var index = require('./routes/index');
 var example = require('./routes/example');
 var users = require('./routes/users');
 var login = require('./routes/login');
+var alexa = require('./routes/alexa');
 
 var app = express();
 
@@ -28,6 +29,7 @@ app.use('/', index);
 app.use('/example', example);
 app.use('/users', users);
 app.use('/login', login);
+app.use('/user', alexa);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
