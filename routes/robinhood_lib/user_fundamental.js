6cdc0b0c7bc1520d3c1ff0737cc7bfb4fbfc8120
VibrantClouds <bluecrecent0@gmail.com>
2017-11-05 08:05:29 -0500

    User Stock Fundamental backend

---
+++ routes/robinhood_lib/user_fundamental.js
@@ -0,0 +1,23 @@
+var db = require('../../database/database.js');
+
+function getUserStockStatus(userid, callback) {
+
+    //Query the SQL database and find out how much money the user currently has
+    var query = "SELECT AvailableMoney, InvestedMoney FROM usertable WHERE (id = " + userid + ");";
+
+    db.get(query, (rows, fields) => {
+        console.log("SQL Response from insert request: " + rows);
+
+        //Turn the account balances into a jsonObject
+        var jsonResponse = {
+
+            AvailableMoney: rows[0].AvailableMoney,
+            InvestedMoney: rows[0].InvestedMoney
+        };
+        
+        callback(jsonResponse);
+
+    });
+}
+
+
