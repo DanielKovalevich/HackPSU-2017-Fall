3c268b398eac0ff363b4498a0d7d450c8be3cb2d
Daniel Kovalevich <dkoval526@gmail.com>
2017-11-05 00:30:25 -0400

    Created model and did some javascript in the buy tab

--- client/public/js/index.js
+++ client/public/js/index.js
@@ -8,4 +8,13 @@ $(document).ready(function() {
     $("#time").html("<i class=\"material-icons\">access_time</i> " + new Date().toString() + "");
     $("#time2").html(date);
     $('.loggout').click(function() {window.location = "./login.html";});
+    $('#myModal').modal('show');
+
+    $(".buy-stock").click(function() {
+        var $item = $(this).closest("tr")   // Finds the closest row <tr> 
+                           .find(".nr")     // Gets a descendent with class="nr"
+                           .text();         // Retrieves the text within <td>
+    
+        alert($item);       // Outputs the answer
+    });
 });
