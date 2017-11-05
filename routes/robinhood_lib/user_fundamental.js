5fbb097963e7b2945010dc3f2a58f07c68c626ef
VibrantClouds <bluecrecent0@gmail.com>
2017-11-05 08:38:32 -0500

    Another function for Alexa Querying

--- routes/robinhood_lib/user_fundamental.js
+++ routes/robinhood_lib/user_fundamental.js
@@ -3,7 +3,7 @@ var db = require('../../database/database.js');
 function getUserStockStatus(userid, callback) {
 
     //Query the SQL database and find out how much money the user currently has
-    var query = "SELECT AvailableMoney, InvestedMoney FROM usertable WHERE (id = " + userid + ");";
+    var query = "SELECT AvailableMoney, InvestedMoney FROM usertable WHERE id = " + userid + ";";
 
     db.get(query, (rows, fields) => {
         console.log("SQL Response from insert request: " + rows);
@@ -18,6 +18,23 @@ function getUserStockStatus(userid, callback) {
         callback(jsonResponse);
 
     });
+}
+
+function getNumberOfStocksInvested(callback) {
+
+    //Query the SQL database to find out this number - Hardcoding in the userid for the time being
+    var query = "SELECT UniqueStockAmount from usertable WHERE id = 1";
+
+    db.get(query, (rows, fields) => {
+
+        var jsonResponse = {
+            
+            UniqueStocks : rows[0].UniqueStockAmount
+
+        };
+
+        callback(jsonResponse);
+    })
 }
 
 
